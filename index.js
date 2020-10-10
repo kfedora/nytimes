78ee3c3f71dde34cb6f499062dd7de5fe14c024a
k <kfedora@protonmail.com>
2020-10-10 07:02:52 -0600

    set viewport to 1920x1080

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ function crawlPage() {
 
         const browser = await puppeteer.launch(options);
         const page = await browser.newPage();
+        await page.setViewport({
+            width: 1920,
+            height: 1080
+        });
         await page.goto("http://nytimes.com", {
             waitUntil: 'networkidle2',
             timeout: 30000
